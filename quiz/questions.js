d5add18890a669ef5fd6f72b3ddd0c35cbece4d6
Uhiene Gift Eleojo <eleojogiftuhiene@gmail.com>
2021-08-13 09:57:46 +0100

    new questios

--- quiz/questions.js
+++ quiz/questions.js
@@ -1,81 +1,193 @@
 // creating an array and passing the number, questions, options, and answers
 let questions = [
-    {
+  {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "Circle of Willis is located at the Interpeduncular fossa at the base of the brain",
+    answer: "True",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
-    ]
+      "True",
+      "False",
+    ],
   },
-    {
+  {
     numb: 2,
-    question: "What does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question:
+      "Which of the following is not a branch of the internal carotid artery?",
+    answer: "Inferior Thyroid artery",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
-    ]
+      "Opthalmic artery",
+      "Posterior communicating artery",
+      "Anterior cerebral artery",
+      "Inferior Thyroid artery",
+    ],
   },
-    {
+  {
     numb: 3,
-    question: "What does PHP stand for?",
-    answer: "Hypertext Preprocessor",
+    question: "What artery gives off the vertebral arteries?",
+    answer: "Subclavian artery",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
-    ]
+      "Subclavian artery",
+      "Common carotid artery",
+      "External Carotid artery",
+      "Thyrocerival artery",
+    ],
   },
-    {
+  {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
+    question:
+      "Which vertebral level marks the bifurcation of the common carotid artery?",
+    answer: "C4",
+    options: ["C1", "C6", "C2", "C4"],
   },
-    {
+  {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question:
+      "The terminal branches of the _______ and _______ arteries all anastomose to form the Circle of Willis",
+    answer: "Vertebral and Internal Carotid",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
+      "Vertebral and Internal Carotid",
+      "Vertebral and External Carotid",
+      "External Carotid and Internal Carotid",
+      "Only Internal Carotid",
+    ],
   },
   {
     numb: 6,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question:
+      "Which of the following is not a branch of the vertebral arteries?",
+    answer: "Posterior communicating artery",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
+      "Meningeal branch",
+      "Anterior and Posterior spinal arteries",
+      "Posterior inferior cerebellar artery",
+      "Posterior communicating artery",
+    ],
   },
-    {
+  {
     numb: 7,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question:
+      "Which of the following is not a branch of the internal carotid arteries?",
+    answer: "Meningeal branch",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
-    ]
+      "Meningeal branch",
+      "Opthalmic artery",
+      "Anterior choroidal artery",
+      "Anterior cerebral artery",
+    ],
+  },
+  //
+  {
+    numb: 8,
+    question: "The main paired constituents of the Circle of Willis are?",
+    answer:
+      "Anterior cerebral, Internal carotid and posterior cerebral arteries",
+    options: [
+      "Only Anterior cerebral and Internal carotid",
+      "Anterior cerebral, Anterior communicating and Internal carotid arteries",
+      "Anterior cerebral, Internal carotid and posterior cerebral arteries",
+      "Posterior cerebral arteries,Internal carotid and Posterior communicating arteries",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "Which of the following is not part of the three cerebral arteries?",
+    answer: "Superior",
+    options: ["Superior", "Anterior", "Middle", "Posterior"],
+  },
+  {
+    numb: 10,
+    question:
+      "The spinal cord is primarily supplied by __ longitudinal arteries",
+    answer: "3",
+    options: ["2", "5", "3", "4"],
+  },
+  {
+    numb: 11,
+    question: "How many posterior spinal arteries are there?",
+    answer: "2",
+    options: ["2", "1", "3", "4"],
+  },
+  {
+    numb: 12,
+    question:
+      "The Anterior cerebral arteries supply the ________ portion of the cerebrum",
+    answer: "Anteromedial",
+    options: ["Posteriomedial", "Lateral", "Anteromedial", "Medial"],
+  },
+  {
+    numb: 13,
+    question: "What cerebral arteries supply the lateral part of the brain?",
+    answer: "Middle cerebral arteries",
+    options: [
+      "Posterior cerebral arteries",
+      "Lateral cerebral arteries",
+      "Anterior cerebral arteries",
+      "Middle cerebral arteries",
+    ],
+  },
+  {
+    numb: 14,
+    question: "What region does the posterior spinal arteries anastomose?",
+    answer: "Pia mater",
+    options: ["Pia mater", "Dura mater", "Arachnoid mater", "Pons"],
+  },
+  {
+    numb: 15,
+    question:
+      "The posterior cerebral arteries supply both the medial and lateral parts of the posterior cerebrum",
+    answer: "True",
+    options: ["True", "False"],
+  },
+  {
+    numb: 16,
+    question:
+      "The basilar artery terminates by bifurcating into the ________ cerebral arteries",
+    answer: "Posterior",
+    options: ["Anterior", "Middle", "Posterior", "Superior"],
+  },
+  {
+    numb: 17,
+    question:
+      "The basilar artery arises from the convergence of the two vertebral arteries",
+    answer: "True",
+    options: ["True", "False"],
+  },
+  {
+    numb: 18,
+    question:
+      "What arteries are connected by the posterior communicating artery?",
+    answer: "Internal carotid and posterior cerebral arteries",
+    options: [
+      "Posterior cerebral and anterior cerebral arteries",
+      "Internal carotid and posterior cerebral arteries",
+      "Ophthalmic and middle cerebral arteries",
+      "Basilar and superior cerebellar arteries",
+    ],
+  },
+  {
+    numb: 19,
+    question:
+      "What arteries are connected by the anterior communicating artery?",
+    answer: "The two anterior cerebral arteries",
+    options: [
+      "The two anterior cerebral arteries",
+      "Internal carotid and anterior cerebral arteries",
+      "Anterior and posterior cerebral arteries",
+      "Anterior cerebral and Anterior choroidal arteries",
+    ],
+  },
+  {
+    numb: 20,
+    question:
+      "Which of the vertebral artery branch supplies the falx cerebelli?",
+    answer: "Meningeal branch",
+    options: [
+      "Meningeal branch",
+      "Anterior spinal artery",
+      "Posterior spinal artery",
+      "Posterior inferior cerebellar artery",
+    ],
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
